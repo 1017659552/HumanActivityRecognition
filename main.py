542c9b428913fd444191086f8b6e64f0bb4c39b4
pinna526 <xl330@126.com>
2021-03-13 19:30:19 +0800

    修改了数据训练的方式，重构代码，调整了部分模型参数，达到不错的学习率，但是有一个输入矩阵没有对齐的bug（不影响结果）。添加了两个新文件(仅学习测试用)。

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import  numpy as np
+import random
 import os
 import cv2
 import matplotlib.pyplot as plt
@@ -9,6 +10,8 @@ import torch
 from torch.optim import Adam,SGD
 import torch.nn as nn
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import torch.utils.data as Data
 
 import network.module_v1 as module_v1
 import network.module_v2 as module_v2
@@ -35,7 +38,8 @@ def getPathImg(path,files):
 '''
 参数设置
 '''
-root_dir = 'D:\\SWUFEthesis\\data\\KTH_preprocess'
+# root_dir = 'D:\\SWUFEthesis\\data\\KTH_preprocess'
+root_dir = '/home/mist/KTH_preprocess'
 labels = ['boxing','handclapping','handwaving','jogging','running','walking']
 n_epochs = 6
 img_width = 120
@@ -43,6 +47,7 @@ img_height = 120
 crop_size = 120
 
 train_img_name = getPathImg(os.path.join(root_dir,'val'),[])
+random.shuffle(train_img_name)  # 乱序
 
 print("(1)正在读取train图像... ...")
 train_imgs = []
@@ -67,7 +72,8 @@ for img_name in tqdm(train_img_name):
     img = img.astype(np.float32)
     train_imgs.append(img)
     # 加载标签
-    label = img_name.split('\\')[-2].split('_')[1]
+    # label = img_name.split('\\')[-2].split('_')[1] ######################本地
+    label = img_name.split('/')[-2].split('_')[1] #######################远程
     train_labels.append(labels.index(label))
 
 assert len(train_labels)==len(train_imgs),'图像和标签数量不一致，退出程序！'
@@ -96,80 +102,161 @@ train_y = torch.from_numpy(train_y)
 # print(train_x.shape)
 # 将验证集转换成torch张量
 val_x_len = len(val_x)
-val_x = val_x.reshape(val_x_len,1,crop_size,crop_size)
+val_x = val_x.reshape(val_x_len,crop_size,1,crop_size)
 val_x = torch.from_numpy(val_x)
 val_y = torch.from_numpy(val_y)
 # print(val_x.shape)
 
+# DataLoader加载数据集
+data_train = DataLoader(Data.TensorDataset(train_x,train_y),batch_size=128,shuffle=True)
+data_test = DataLoader(Data.TensorDataset(val_x,val_y),batch_size=128,shuffle=True)
+
 module_v1 = module_v1.Net()
 # module_v1 = module_v2.LeNet()
 # 定义优化器
-optimizer = Adam(module_v1.parameters(),lr = 0.07)
+# optimizer = Adam(module_v1.parameters(),lr = 0.001)
+# optimizer = SGD(module_v1.parameters(),lr = 1e-5)
+optimizer = SGD(module_v1.parameters(), lr=0.01, weight_decay=1e-6, momentum=0.9, nesterov=True)
+
 # 定义loss函数
 criterion = nn.CrossEntropyLoss()
 # 检查gpu
 # 检查gpu是否可用，否则使用cpu
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# print("use device:",device)
-# torch.cuda.empty_cache()
-#
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("use device:",device)
+
 # if torch.cuda.is_available():
 #     module_v1.to(device)
 #     criterion.to(device)
 
 print("使用的模型如下：")
-print(module_v1)
+# print(module_v1)
+
+for epoch in range(1, 10):
+    train_loss, valid_loss = [], []
+    correct = 0
+    total = 0
+    module_v1.train()  # 训练开始
+    for data, target in data_train:
+        optimizer.zero_grad()  # 梯度置0
+        output = module_v1(data)
+        loss = criterion(output, target)  # 计算损失
+        loss.backward()  # 反向传播
+        optimizer.step()  # 更新参数
+        train_loss.append(loss.item())
+        # 输出正确率
+        total += target.size(0)
+        _, preds_tensor = torch.max(output, 1)
+        correct += np.squeeze((preds_tensor == target).sum().numpy())
+        print(correct / total)
+
+    # net.eval()  # 验证开始
+    # for data, target in validloader:
+    #     output = net(data)
+    #     loss = loss_function(output, target)
+    #     valid_loss.append(loss.item())
+
+    print("Epoch:{}, Training Loss:{}, Valid Loss:{}".format(epoch, np.mean(train_loss), np.mean(valid_loss)))
+print("======= Training Finished ! =========")
+
+print("Testing Begining ... ")  # 模型测试
+total = 0
+correct = 0
+for i, data_tuple in enumerate(data_test, 0):
+    data, labels = data_tuple
+    output = module_v1(data)
+    _, preds_tensor = torch.max(output, 1)
+
+    total += labels.size(0)
+    correct += np.squeeze((preds_tensor == labels).sum().numpy())
+print("Accuracy : {} %".format(correct / total))
 
 
-def train(epoch):
-    module_v1.train()
-    tr_loss = 0
-    #获取训练集和验证集
-    x_train,y_train = Variable(train_x),Variable(train_y)
-    x_val,y_val = Variable(val_x),Variable(val_y)
-    # 转换为gpu格式
-    # if torch.cuda.is_available():
-    #     x_train = x_train.to(device)
-    #     y_train = y_train.to(device)
-    #     x_val = x_val.to(device)
-    #     y_val = y_val.to(device)
-    #这里转换标签的格式，否则会报错 expected scalar type Long but found Int
-    y_train = y_train.type(torch.LongTensor)
-    y_val = y_val.type(torch.LongTensor)
-
-    # 清除梯度
-    optimizer.zero_grad()
-    # 预测训练与验证集
-    output_train = module_v1(x_train)
-    output_val = module_v1(x_val)
-    # 计算验证集和训练集损失
-    loss_train = criterion(output_train,y_train)
-    loss_val = criterion(output_val,y_val)
-    train_losses.append(loss_train)
-    val_losses.append(loss_val)
-    # 更新权重
-    loss_train.backward()
-    optimizer.step()
-    tr_loss = loss_train.item()
-    if(epoch%2==0):
-        #输出验证集loss
-        print('Epoch:',epoch+1,'  loss:',loss_val)
 
 
-train_losses = []
-val_losses = []
-for epoch in tqdm(range(n_epochs)):
-    train(epoch)
 
-plt.plot(train_losses,label = 'Training loss')
-plt.plot(val_losses,label='Validation loss')
-plt.legend()
-plt.show()
 
-with torch.no_grad():
-    output = module_v1(train_x)
-softmax = torch.exp(output)
-prob = list(softmax.numpy())
-predictions = np.argmax(prob,axis = 1)
-
-print(accuracy_score(train_y,predictions))
+#
+# def cal_accuracy(model, x_test, y_test, samples=10000):
+#
+#     y_pred = model(x_test[:samples])
+#     y_pred_ = list(map(lambda x: np.argmax(x), y_pred.data.numpy()))
+#     acc = sum(y_pred_ == y_test.numpy()[:samples]) / samples
+#     return acc
+#
+# def train(epoch):
+#     # module_v1.train()
+#     # tr_loss = 0
+#     # #获取训练集和验证集
+#     # x_train,y_train = Variable(train_x),Variable(train_y)
+#     # x_val,y_val = Variable(val_x),Variable(val_y)
+#     # #这里转换标签的格式，否则会报错 expected scalar type Long but found Int
+#     # y_train = y_train.type(torch.LongTensor)
+#     # y_val = y_val.type(torch.LongTensor)
+#     #
+#     # # 转换为gpu格式
+#     # # if torch.cuda.is_available():
+#     # #     x_train = x_train.to(device)
+#     # #     y_train = y_train.to(device)
+#     # #     x_val = x_val.to(device)
+#     # #     y_val = y_val.to(device)
+#     # # 清除梯度
+#     # optimizer.zero_grad()
+#     # # 预测训练与验证集
+#     # output_train = module_v1(x_train)
+#     # output_val = module_v1(x_val)
+#     # # 计算验证集和训练集损失
+#     # loss_train = criterion(output_train,y_train)
+#     # loss_val = criterion(output_val,y_val)
+#     # train_losses.append(loss_train)
+#     # val_losses.append(loss_val)
+#     # # 更新权重
+#     # loss_train.backward()
+#     # optimizer.step()
+#     # tr_loss = loss_train.item()
+#     # if(epoch%2==0):
+#     #     #输出验证集loss
+#     #     print('Epoch:',epoch+1,'  loss:',loss_val)
+#     ##################################################
+#     # 另一种写法
+#     acc_data = []
+#     loss_data = []
+#     for index,(x_data,y_data) in enumerate(data_train):
+#         prediction = module_v1(torch.unsqueeze(x_data,dim = 1))
+#         loss = criterion(prediction,y_data)
+#         print('No.%s,loss=%.3f' % (index + 1, loss.data.numpy()))
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         loss_val = loss.data.numpy()
+#         if epoch==0:
+#             softmax = torch.exp(prediction)
+#             prob = list(softmax.numpy())
+#             predictions = np.argmax(prob,axis = 1)
+#             acc = accuracy_score(y_data,predictions)
+#             acc_data.append(acc)
+#             loss_data.append(loss_val)
+#         print('No.%s,loss=%.3f' % (epoch + 1, loss_val))
+#         print('acc = ',acc)
+#
+#
+#
+# # train_losses = []
+# # val_losses = []
+# for epoch in tqdm(range(n_epochs)):
+#     train(epoch)
+#
+# acc=cal_accuracy(module_v1,torch.unsqueeze(val_x,dim=1),val_y,samples=10000)
+#
+# # plt.plot(train_losses,label = 'Training loss')
+# # plt.plot(val_losses,label='Validation loss')
+# # plt.legend()
+# # plt.show()
+# #
+# # with torch.no_grad():
+# #     output = module_v1(train_x)
+# # softmax = torch.exp(output)
+# # prob = list(softmax.numpy())
+# # predictions = np.argmax(prob,axis = 1)
+# #
+# print(accuracy_score(train_y,predictions))
